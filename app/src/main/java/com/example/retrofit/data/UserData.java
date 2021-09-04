3514951c03c34a25464600944b30c30e0c2abd8e
inseong04 <ois0531@naver.com>
2021-09-04 23:08:22 +0900

    add UserData

---
+++ app/src/main/java/com/example/retrofit/data/UserData.java
@@ -0,0 +1,19 @@
+package com.example.retrofit.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class UserData {
+    @SerializedName("email")
+    public String email;
+
+    @SerializedName("password")
+    public String password;
+/*
+    @SerializedName("token")
+    public String token;*/
+
+    public UserData(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
